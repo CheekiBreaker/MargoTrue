9da0b1e11143a93c4bc68fa03ef32c3e9a6260a3
CheekiBreaker <trubachev199930@gmail.com>
2024-09-05 09:06:17 +0700

    edit animation on about page

---
+++ aboutjs.js
@@ -0,0 +1,27 @@
+// about page
+
+const aboutBlock = document.querySelectorAll(".comfortBlock");
+
+window.addEventListener("scroll", (e) => {
+  if (pageYOffset >= 820) {
+    aboutBlock.forEach((el) => {
+      el.classList.add("op");
+    });
+  } else {
+    aboutBlock.forEach((el) => {
+      el.classList.remove("op");
+    });
+  }
+});
+
+var items = document.querySelectorAll("ul.offerUl li");
+window.addEventListener("scroll", (e) => {
+  if (pageYOffset = 500) {
+    items.forEach((el, index) => {
+      el.style.opacity = 0;
+      setTimeout(() => {
+        el.style.opacity = 1;
+      }, index * 400);
+    });
+  }
+});
